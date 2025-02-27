b6a66daeaeb53833a7bf0467b319a9d3dbb3f698
Milleand <140829447+Milleand@users.noreply.github.com>
2025-02-27 11:15:30 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const user1 = {
+    address: {
+        city: "New York"
+    } };
+
+const user2 = {
+
+};
+
+// TODO: Define getUserCity here.
+function getUserCity(user){
+    return user?.address?.city
+}
+
+console.log(getUserCity(user1)); // "New York"
+console.log(getUserCity(user2)); // "Unknown"
+
